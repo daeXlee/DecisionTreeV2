6f50d7301078af7338e233c4f297ad8792a4f2b3
daeXlee <daewonxlee@gmail.com>
2021-08-28 14:30:26 -0500

    Add Split Attribute

--- ml/src/main/java/com/ml/DecisionTree.java
+++ ml/src/main/java/com/ml/DecisionTree.java
@@ -1,12 +1,20 @@
 package com.ml;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Hashtable;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
+
+import javax.management.InstanceAlreadyExistsException;
+
+import org.apache.commons.lang3.math.NumberUtils;
+import org.apache.commons.lang3.tuple.MutablePair;
+import org.apache.commons.lang3.tuple.Pair;
 
 public class DecisionTree {
 
@@ -35,53 +43,153 @@ public class DecisionTree {
     private Node root;
     private List<String[]> trainData;
     private List<AttributeInfo> attributeInfoList;
-    private AttributeInfo classAttribute;
     private int classIndex;  // Index of Classification Attribute in the dataset
+    private double classThreshold; // Only set if Classification is Continuous
 
     // Used to determine when a node is a leaf
     final int MAX_PER_LEAF = 3;
     final int MAX_DEPTH = 20;
 
+    public DecisionTree(List<String[]> trainData, List<AttributeInfo> attributeInfoList, int classIndex) {
+        this.trainData = trainData;
+        this.attributeInfoList = attributeInfoList;
+        this.classIndex = classIndex;
+    }
+
+    // Build Tree
+    private Node buildTree() {
+        // Initial Best Split
+        Pair<Integer, Double> splitInformation = getSplitAttribute(trainData);
+        Node root = new Node("", splitInformation.getLeft(), splitInformation.getRight());
+
+        return root;
+    }
+
+    // Get Best Split
+    public Pair<Integer, Double> getSplitAttribute(List<String[]> data) {
+        double maxInfoGain = 0.0;
+        double gainRatio;
+        int maxInfoIndex = 0;
+        double maxInfoThreshold = 0.0;
+
+        for (int i = 0; i < attributeInfoList.size(); i++) {
+            // Skip Classification Attribute
+            if (i == this.classIndex) continue;
+
+            if (attributeInfoList.get(i).isContinous()) {
+                for (String value : attributeInfoList.get(i).getValues()) {
+                    if (NumberUtils.isCreatable(value)) {
+                        gainRatio = getGainRatio(data, i, NumberUtils.toDouble(value));
+                        
+                        if (maxInfoGain < gainRatio) {
+                            maxInfoGain = gainRatio;
+                            maxInfoIndex = i;
+                            maxInfoThreshold = NumberUtils.toDouble(value);
+                        }
+                    }
+                }
+            }
+            else {
+                gainRatio = getGainRatio(data, i, 0);  // Nominal Attr. don't care about threshold
+                
+                if (maxInfoGain < gainRatio) {
+                    maxInfoGain = gainRatio;
+                    maxInfoIndex = i;
+                }
+            }
+        }
+
+        return new MutablePair<>(maxInfoIndex, maxInfoThreshold);
+    }
+
+    // TODO: Implement when Classification is Continuous
     public double getGainRatio(List<String[]> data, int attributeIndex, double threshold) {
         AttributeInfo attributeInfo = attributeInfoList.get(attributeIndex);
+        boolean isContinous = attributeInfo.isContinous();
         // Key: {Attr. Option}, Value: {Key: Class Option Value: Count}
         HashMap<String, Map<String, Long>> labelCounts = new HashMap<>();
         initializeLabelMap(labelCounts, attributeInfo);
 
         for (String[] instance : data) {
-            if (attributeInfo.isContinous()) {
+            String attributeOption;
+            String classDecisionOption = instance[classIndex];
 
+            if (isContinous) {
+                double instanceAttributeValue = NumberUtils.toDouble(instance[attributeIndex]);
+                attributeOption = instanceAttributeValue <= threshold ? "<=" : ">";
             }
             else {
-                String key = instance[attributeIndex];
-                Map<String, Long> innerMap = labelCounts.get(key);
-                
-                innerMap.put(instance[classIndex], innerMap.get(instance[classIndex]) + 1);
-                labelCounts.put(key, innerMap);
+                attributeOption = instance[attributeIndex];
+            }
+
+            Map<String, Long> innerClassMap = labelCounts.get(attributeOption);
+            innerClassMap.put(classDecisionOption, innerClassMap.get(classDecisionOption) + 1);
+        }
+
+        HashMap<String, Long> classLabelCounts = new HashMap<>();
+
+        for (Map<String, Long> classCountMap : labelCounts.values()) {
+            for (Map.Entry<String, Long> classCount : classCountMap.entrySet()) {
+                Long count = classLabelCounts.get(classCount.getKey());
+
+                count = count == null ? classCount.getValue() : count + classCount.getValue();
+                classLabelCounts.put(classCount.getKey(), count);
             }
         }
 
-        return 0;
+        Long total = classLabelCounts.values().stream().collect(Collectors.summingLong(Long::longValue));
+        double classEntropy = entropy(classLabelCounts.values());
+        double attributeGainCalc = 0.0;
+        List<Long> attributeTotalCounts = new ArrayList<>();
+
+        for (Map<String, Long> classCountMap : labelCounts.values()) {
+            Long attributeTotalCount = classCountMap.values().stream().collect(Collectors.summingLong(Long::longValue));
+            double attributeEntropy = entropy(classCountMap.values());
+
+            attributeTotalCounts.add(attributeTotalCount);
+            attributeGainCalc += ((double)attributeTotalCount / total) * attributeEntropy;
+
+        }
+
+        double gain = classEntropy - attributeGainCalc;
+        double splitInfo = entropy(attributeTotalCounts);
+
+        return gain / splitInfo;
     }
 
     private void initializeLabelMap(HashMap<String, Map<String, Long>> map, AttributeInfo attributeInfo) {
-        Map<String, Long> classOptionMap = classAttribute.getValues().stream().collect(Collectors.toMap(s -> s, s -> 0l));
+        AttributeInfo classAttribute = attributeInfoList.get(classIndex);
+        Map<String, Long> classOptionMap;
 
-        for (String attributeOption : attributeInfo.getValues()) {
-            map.put(attributeOption, classOptionMap);
+        // Initialize Inner Class Decision Map
+        if (classAttribute.isContinous()) {
+            classOptionMap = new HashMap<>();
+            classOptionMap.put("<=", 0l);
+            classOptionMap.put(">", 0l);
+        }
+        else {
+            classOptionMap =  classAttribute.getValues().stream().collect(Collectors.toMap(s -> s, s -> 0l));
+        }
+
+        if (attributeInfo.isContinous()) {
+            map.put("<=", new HashMap<>(classOptionMap));
+            map.put(">", new HashMap<>(classOptionMap));
+        }
+        else {
+            attributeInfo.getValues().forEach(s -> map.put(s, new HashMap<>(classOptionMap)));
         }
     }
 
-    public double entropy(long[] labelCounts) {
-        double sum = 0;
-        long total = Arrays.stream(labelCounts).sum();
+    public double entropy(Collection<Long> collection) {
+        double sum = 0.0;
+        Long total = collection.stream().collect(Collectors.summingLong(Long::longValue));
 
         if (total == 0) {
             return sum;
         }
 
-        for (long labelCount : labelCounts) {
-            sum -= (double)(labelCount / total) * log2((double)(labelCount / total));
+        for (long labelCount : collection) {
+            sum -= (double)labelCount / total * log2((double)labelCount / total);
         }
 
         return sum;
